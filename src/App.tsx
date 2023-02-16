5566ce1f7ac85904f0bbf4d360a79165cb875297
albertshops <alistoresali@gmail.com>
2023-02-16 11:15:05 +0000

    Add negation

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,91 @@
 import { useEffect, useState } from "react"
 
-const pronouns = [
-  ["mimi ni", "I"],
-  ["wewe u", "you"],
-  ["yeye a", "he/she/it"],
-  ["sisi tu", "we"],
-  ["wao wa", "they"],
-  ["ninyi m", "you (plural)"],
-]
+function getRandom<T>(arr: T[]) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 
-const tenses = [
-  ["na", "present"],
-  ["li", "past"],
-  ["ta", "future"],
-  ["me", "perfect"],
-]
+const pronouns = {
+  I: "mimi",
+  you: "wewe",
+  it: "yeye",
+  we: "sisi",
+  they: "wao",
+  "you all": "ninyi",
+} as const
 
-const verbs = [
-  ["kula", "eat"],
-  ["pika", "cook"],
-  ["kula", "eat"],
-  ["soma", "read"],
-  ["penda", "love"],
-  ["enda", "go"],
-  ["toka", "leave"],
-  ["ona", "see"],
-]
+const subjectPrefixes = {
+  positive: {
+    I: "ni",
+    you: "u",
+    it: "a",
+    we: "tu",
+    they: "wa",
+    "you all": "m",
+  },
+  negative: {
+    I: "si",
+    you: "hu",
+    it: "ha",
+    we: "hatu",
+    they: "hawa",
+    "you all": "ham",
+  },
+}
+
+const tenses = {
+  present: {
+    positive: "na",
+    negative: "",
+  },
+  past: {
+    positive: "li",
+    negative: "ku",
+  },
+  future: {
+    positive: "ta",
+    negative: "ta",
+  },
+  perfect: {
+    positive: "me",
+    negative: "ja",
+  },
+} as const
+
+const verbs = {
+  eat: "kula",
+  cook: "pika",
+  read: "soma",
+  love: "penda",
+  go: "enda",
+  see: "ona",
+  sing: "imba",
+  play: "cheza",
+  "come from": "toka",
+}
+
+type Pronoun = keyof typeof pronouns
+type Polarity = "positive" | "negative"
+type Tense = keyof typeof tenses
+type Verb = keyof typeof verbs
+type Sentence = {
+  pronoun: Pronoun
+  polarity: Polarity
+  tense: Tense
+  verb: Verb
+}
 
 function App() {
-  const [sentence, setSentence] = useState([0, 0, 0])
+  const [sentence, setSentence] = useState<Sentence>()
   const [showAnswer, setShowAnswer] = useState(false)
 
-  const pronoun = pronouns[sentence[0]]
-  const tense = tenses[sentence[1]]
-  const verb = verbs[sentence[2]]
-
-  const s = sentence[0] == 2 ? "s" : ""
-  const e = s && sentence[2] == 5 ? "e" : ""
-  const english = `${pronoun[1]} ${verb[1]}${e}${s} (${tense[1]})`
-  const swalihi = `${pronoun[0]}${tense[0]} ${verb[0]}`
-
   const next = () => {
-    setSentence([
-      Math.floor(Math.random() * pronouns.length),
-      Math.floor(Math.random() * tenses.length),
-      Math.floor(Math.random() * verbs.length),
-    ])
+    setSentence({
+      pronoun: getRandom(Object.keys(pronouns) as Pronoun[]),
+      polarity: getRandom(["positive", "negative"]),
+      tense: getRandom(Object.keys(tenses) as Tense[]),
+      verb: getRandom(Object.keys(verbs) as Verb[]),
+    })
+
     setShowAnswer(false)
   }
 
@@ -53,12 +93,26 @@ function App() {
     next()
   }, [])
 
+  if (!sentence) return <></>
+
+  const { pronoun, polarity, tense, verb } = sentence
+  const english = `(${polarity}) ${pronoun} ${verb} (${tense})`
+
+  let swahiliVerb = verbs[verb]
+  if (
+    swahiliVerb.endsWith("a") &&
+    tense === "present" &&
+    polarity === "negative"
+  )
+    swahiliVerb = swahiliVerb.slice(0, -1) + "i"
+  const swahili = `${pronouns[pronoun]} ${subjectPrefixes[polarity][pronoun]}${tenses[tense][polarity]}${swahiliVerb}`
+
   return (
     <div className="h-full w-full flex justify-between items-center flex-col gap-4 text-lg p-8">
       <div />
       <div className="flex flex-col gap-2 items-center">
         <div className="">{english}</div>
-        <div>{showAnswer ? swalihi : "-"}</div>
+        <div>{showAnswer ? swahili : "-"}</div>
       </div>
       <div className="flex gap-4">
         <Button onClick={() => setShowAnswer(true)}>answer</Button>
